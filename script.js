c7edfd96e4efdf5fdd51a7c24f9bf64338c9f90a
frohrich <107487678+frohrich@users.noreply.github.com>
2026-01-14 16:04:03 -0300

    finalização do projeto focusone

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 console.log("Javacript funcionando")
 
 const botao = document.getElementById("comprar");
+const mensagem = document.getElementById("mensagem");
 
 botao.addEventListener("click", function () {
-    alert("Produto adicionado ao carrinho");
+    mensagem.textContent = "Produto adicionado ao carrinho!";
+    botao.textContent = "Produto Adicionado";
+    botao.classList.add(adicionado);
 });
+
